6abab4a29cea0f30d7174a684ca1f971866870f9
Camille Coutens <camille.coutens@gmail.com>
2018-10-09 18:04:54 +0200

    ex5 filter ok

--- filter/ex5.js
+++ filter/ex5.js
@@ -38,7 +38,10 @@ Exemple d'entrée:
 
  */
 
-function searchWordFilter(items, search) {
+const searchWordFilter = (items, search) => {
+  return items.filter(x => {
+    return x.toLowerCase().includes(search)
+  })
 }
 
 // Ne pas modifier l'export
