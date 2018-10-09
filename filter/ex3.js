a63099e538e60a8a1c9566ef9f805ba60defede3
Camille Coutens <camille.coutens@gmail.com>
2018-10-09 17:58:44 +0200

    ex5 filter ok

--- filter/ex3.js
+++ filter/ex3.js
@@ -23,7 +23,10 @@ Sortie attendue:
 
  */
 
-function keepStarks(names) {
+const keepStarks = (names) => {
+  return names.filter(x => {
+    return x.endsWith(" Stark")
+  })
 }
 
 // Ne pas modifier l'export
